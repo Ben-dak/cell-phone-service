bd7afcee3c0835fefbb20d47f4d56a93e9058ac3
Dominick <dominickfrench.oe@gmail.com>
2025-09-30 15:41:35 -0500

    Second commit

--- src/main/java/CellPhone.java
+++ src/main/java/CellPhone.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class CellPhone {
+    private String model;
+    private String phoneNumber;
+
+    public CellPhone() {
+        model = "";
+        phoneNumber = "";
+
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+
+    }
 }
+
