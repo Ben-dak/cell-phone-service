8bac7238ca0bb56e26ab8ca4536caf001069ca38
Dominick <dominickfrench.oe@gmail.com>
2025-10-02 09:28:37 -0500

    Third commit

--- src/main/java/CellPhone.java
+++ src/main/java/CellPhone.java
@@ -1,20 +1,39 @@
 public class CellPhone {
-    private String model;
+
+    private String owner;
     private String phoneNumber;
+    private String serialNumber;
+    private String carrier;
 
     public CellPhone() {
-        model = "";
-        phoneNumber = "";
-
-    }
-
-    public String getModel() {
-        return model;
+        this.owner = "";
+        this.phoneNumber = "";
+        this.serialNumber = "";
+        this.carrier = "";
+        this.owner = "";
     }
 
     public void setModel(String model) {
-        this.model = model;
+        this.owner = model;
+    }
 
+    public String getModel() {
+        return this.owner;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setOwner(String n) {
+        this.owner = n;
+    }
+
+    public String getOwner() {
+        return
     }
 }
-
---
+++ src/main/java/CellPhoneApplication.java
@@ -0,0 +1,10 @@
+public class CellPhoneApplication {
+
+    public static void main(String[] args) {
+        CellPhone phone1 = new CellPhone();
+        phone1.setOwner("Dominick");
+        CellPhone.setmodel()
+
+        System.out.println("phone 1 owner: " + phone1.getOwner());
+    }
+}
